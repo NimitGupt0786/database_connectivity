de7bf147f77c12b49315b4c6937e4a2d83f6d0cf
Nimit_Gupta <110985866+NimitGupt0786@users.noreply.github.com>
2023-07-08 17:06:39 +0530

    Update connectivity.py

--- connectivity.py
+++ connectivity.py
@@ -1,20 +1,22 @@
 import mysql.connector
 
 connection = 
-mysql.connector.connect(host='127.0.0.1',username='root',passwd='Gupta6969',database='students')
+mysql.connector.connect(host='127.0.0.1',username='root',passwd='Gupta6969',database='students') #it returns the connection after authenticating the username and password
 
-cursor = connection.cursor()
+cursor = connection.cursor()                    #it acts a pointer to the database which we have connected to
 
-cursor.execute("select * from student")
-for i in cursor:
+cursor.execute("select * from student")         #it stores the result of the command executed
+
+for i in cursor:                                #it is used to print the result stored in 8th line
     print(i,'all')
 
 cursor.execute("select * from student where name='nimit'")
+
 for i in cursor:
     print(i,'name')
 
 try:
-    sql = "INSERT INTO student (name, college) VALUES (%s, %s)"
+    sql = "INSERT INTO student (name, college) VALUES (%s, %s)" # it is the practical implementation how commands canbe run and exectued and make changes in original database 
     values = [('Nitish', 'thapar')]
     cursor.executemany(sql, values)
 
@@ -24,7 +26,8 @@ except mysql.connector.Error as error:
     connection.rollback()
 
 try:
-    sql = "delete from student where name = 'Nitish'"
+    sql = "delete from student where name = 'Nitish'"    # it is the practical implementation how commands canbe run and exectued and make changes in original database 
+    values = [('Nitish', 'thapar')]
     cursor.execute(sql)
 
     connection.commit()
